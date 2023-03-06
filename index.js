bd02b5706be2ac293cec6ca89c8263ed93e9f147
DuongHaAnh <duonghaanhteen8@gmail.com>
2023-03-06 20:59:43 +0700

    buoi1

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//let a = 10;
+//a = 100;
+//let b = 9
+//a = b
+ 
+//let name = "Anh";
+//let massage = "";
+//massage = name
+//console.log(massage)
+//console.log(a)
+//let info {"name":"Anh","age":"19"} 
+//console.log(info)
+//let s = 0
+//let a = 6
+//let b = 2
+//let c = 3
+//s = ((19+a)*b)**c
+//console.log(s)
+//let a = 10
+//let b = 10
+//let c = 10
+//tuong duong let a,b,c = 10 ( chu y - chua biet ntn)
+ //let a = 10;
+ //a = a + 10;
+ // tuong duong a+=10
+//a+=10
+//a-=5
+console.log(a)
+
+app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+})
